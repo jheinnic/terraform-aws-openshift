765672fa003727a36f8dfbe8114ad25214f320c9
John Heinnickel <jheinnic@gmail.com>
2020-08-18 17:06:28 -0700

    Installer Bastion

--- modules/openshift/08-bastion.tf
+++ modules/openshift/08-bastion.tf
@@ -6,13 +6,14 @@ resource "aws_eip" "bastion_eip" {
 
 resource "aws_spot_instance_request" "bastion" {
   ami                  = "${data.aws_ami.amazonlinux.id}"
-  instance_type        = "t2.small"
+  instance_type        = "${var.bastion_instance_type}"
   iam_instance_profile = "${aws_iam_instance_profile.bastion-instance-profile.id}"
   subnet_id            = "${aws_subnet.public-subnet.id}"
   spot_price           = "${var.bastion_spot_price}"
   spot_type            = "persistent"
   wait_for_fulfillment = true
   instance_interruption_behaviour = "stop"
+  associate_public_ip_address = false
 
   vpc_security_group_ids = [
     "${aws_security_group.openshift-vpc.id}",
@@ -26,7 +27,7 @@ resource "aws_spot_instance_request" "bastion" {
   tags = "${merge(
     local.common_tags,
     map(
-      "Name", "OpenShift Bastion"
+      "Name", "OpenShift Installer Bastion"
     )
   )}"
 }
